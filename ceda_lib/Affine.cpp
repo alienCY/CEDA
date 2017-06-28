a3225d24202ec29041a6fdd76db6f40333ed3607
George Christodoulou <georgethebest@outlook.com.gr>
2017-06-28 21:54:21 +0300

    Add files via upload

---
+++ ceda_lib/Affine.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include "generalParameters.hpp"
+#include "generalFunctions.hpp"
+#include "Affine.hpp"
+
+void ceda::Affine::setKey(int aKey, int bKey, int alphabetLength)
+{
+    assert( 0 <= aKey && 0<= bKey && aKey < alphabetLength && bKey < alphabetLength && "Range");
+    assert(general::gcd(aKey) == 1 && "aKey and 26(length of alphabet) aren't coprime");
+    //if it's ok, set the key
+    m_aKey = aKey;
+    m_bKey = bKey;
+}
+
+
+void ceda::Affine::printTable(const std::string &alphabet)
+{
+    std::cout << "Decrypted alphabet: ";
+    general::printAlphabet(Align::HORIZONTAL);
+    std::cout << "Encrypted alphabet: ";
+    general::printAlphabet(Align::HORIZONTAL);
+}
--- ceda_lib/Affine.hpp
+++ ceda_lib/Affine.hpp
@@ -1,21 +1,37 @@
 #ifndef AFFINE_HPP
 #define AFFINE_HPP
 #include <string>
-#include "generalFunctions.hpp"
+#include "generalParameters.hpp"
 
 namespace ceda
 {
     class Affine
     {
     private:
-        std::string m_encryptedText;
-        std::string m_decryptedText;
+        const std::string m_encryptedText;
+        const std::string m_decryptedText;
+        const std::string m_text;
+        const std::string m_alphabet;
+        int m_aKey;
+        int m_bKey;
 
     public:
-        const std::string& getEncryptedText() { return m_encryptedText; }
-       
-        
+        Affine(const std::string &text,int aKey,int bKey,const std::string &alphabet=alphabet_upper)
+            : m_text(text), m_alphabet(alphabet)                       
+        {
+            setKey(aKey, bKey);
+        }
 
+        Affine(const std::string &text)
+            : m_text(text), m_aKey(1), m_bKey(0)       {               }
+
+        void setKey(int,int,int=26); //3rd argument is alphabetLength
+        void encrypt();
+        void decrypt();
+        void printTable(const std::string&);
+
+        std::string getEncryptedText() const { return m_encryptedText; }
+        std::string getDecryptedText() const { return m_encryptedText; }
 
     };
 }
